97cfc85583f2f77d54c1e354c27df581995f9155
Miss-Hedgehog <271551725@qq.com>
2022-09-07 09:28:59 +0800

    Initial commit

---
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -0,0 +1,30 @@
+package com.jnu.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewHelloWorld;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewHelloWorld=this.findViewById(R.id.text_hello_world);
+        String strHellWorld=textViewHelloWorld.getText().toString();
+        //Log.i("MainActivity",strHelloWorld);
+        textViewHelloWorld.setText("test");
+        textViewHelloWorld.setText(R.string.hello_world2);
+
+        String strHelloWorld2=this.getResources().getText(R.string.hello_world2).toString();
+        textViewHelloWorld.setText(strHelloWorld2);
+
+
+
+    }
+}

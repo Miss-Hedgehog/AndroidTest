aada5bb2f7eae8a034ce3b861042a9bcee39c619
Miss-Hedgehog <271551725@qq.com>
2022-09-07 09:46:34 +0800

    Initial commit

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -2,6 +2,7 @@ package com.jnu.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.util.Log;
@@ -11,13 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textViewHelloWorld;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewHelloWorld=this.findViewById(R.id.text_hello_world);
         String strHellWorld=textViewHelloWorld.getText().toString();
-        Log.i("MainActivity",strHelloWorld);
         textViewHelloWorld.setText("test");
         textViewHelloWorld.setText(R.string.hello_world2);
 

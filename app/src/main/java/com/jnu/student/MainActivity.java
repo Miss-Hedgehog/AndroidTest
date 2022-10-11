30ae912468714373aa5cfddb0dfc9fe5937b6d18
Miss-Hedgehog <271551725@qq.com>
2022-10-11 17:54:12 +0800

    按钮事件（交换两个文本框的值）

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,39 +1,40 @@
 package com.jnu.student;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView textViewHelloWorld;
+public class MainActivity extends AppCompatActivity{
+    private TextView textViewHelloWorld1;
+    private TextView textViewHelloWorld2;
+    private Button  changeText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewHelloWorld=findViewById(R.id.text_view_hello_world);
-
-        Button buttonChinese=findViewById(R.id.button_chinese);
-        Button buttonEnglish=findViewById(R.id.button_english);
-        buttonChinese.setOnClickListener(new View.OnClickListener() {
+        textViewHelloWorld1=findViewById(R.id.text_view_hello_world1);//find text1
+        textViewHelloWorld2=findViewById(R.id.text_view_hello_world2);//find text2
+        //changeText=(Button)findViewById(R.id.button_change);//find button
+        changeText=findViewById(R.id.button_change);
+        changeText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textViewHelloWorld.setText(R.string.string_chinese);
+                if(view==changeText) {
+                    String str1 = textViewHelloWorld1.getText().toString();
+                    String str2 = textViewHelloWorld2.getText().toString();
+                    textViewHelloWorld2.setText(str1);
+                    textViewHelloWorld1.setText(str2);
+                }
             }
         });
-        buttonEnglish.setOnClickListener(new EnglishClickListener());
+
 
 
     }
 
-    private class  EnglishClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            textViewHelloWorld.setText(R.string.string_english);
 
-        }
-    }
 }
